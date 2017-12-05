d7e12a284aa75aea861dfbdbba3c9175a0002b02
jackphillips7764 <jackiscool20@gmail.com>
2017-12-05 14:56:16 -0500

    document nbt load_test

--- world/load_test.go
+++ world/load_test.go
@@ -7,17 +7,20 @@ import (
 )
 
 func TestLoad(t *testing.T) {
+	//load world files directory
 	path, err := minecraft.NewFilePath("./nw")
 	if err != nil {
 		fmt.Println("err")
 		fmt.Println(err)
 	}
+	//create new level
 	test2, err := minecraft.NewLevel(path)
 	if err != nil {
 		fmt.Println("err")
 		fmt.Println(err)
 	}
 	fmt.Println(test2)
+	//get chunk from level @ 0,0
 	fmt.Println("chunk")
 	test3, err := test2.GetChunk(0, 0, false)
 	if err != nil {
@@ -27,6 +30,7 @@ func TestLoad(t *testing.T) {
 	fmt.Println(test3)
 	var maxheight int32
 	fmt.Println("test")
+	//get max height for slice print
 	for i := 0; i < 16; i++ {
 		if test3.GetHeight(int32(i), 0) > maxheight {
 			maxheight = test3.GetHeight(int32(i), 1)
@@ -35,11 +39,13 @@ func TestLoad(t *testing.T) {
 	fmt.Println("test")
 	fmt.Println(maxheight)
 	mapslice := make([][]uint16, 16)
+	//makes array mapslece
 	for i := range mapslice {
 		mapslice[i] = make([]uint16, maxheight)
 	}
 	fmt.Println("test")
 	fmt.Println(len(mapslice[0]))
+	//check cordinates and load in block data
 	for i := 0; i < 16; i++ {
 		for j := 0; int32(j) < maxheight; j++ {
 			block, _ := test2.GetBlock(int32(i), int32(j), 1)
@@ -49,6 +55,7 @@ func TestLoad(t *testing.T) {
 	}
 	fmt.Println("test3")
 	fmt.Println(mapslice)
+	//print out slice
 	for i := maxheight - 1; i > -1; i-- {
 		for j := range mapslice {
 			fmt.Printf("%2d ", mapslice[j][i])
@@ -57,8 +64,8 @@ func TestLoad(t *testing.T) {
 	}
 	fmt.Println("test4")
 
+	//additional debug checks
 	fmt.Println(test3.GetHeight(0, 0))
-
 	test4, err := test2.GetBlock(0, 0, 0)
 	if err != nil {
 		fmt.Println("err")
